6a6ccc27bdf7d5d9974b33a2543413f62afc9ff0
Nikita Kolmogorov <backmeupplz@gmail.com>
2017-10-24 02:41:37 +0000

    remove github.sh

--- github.sh
+++
@@ -1,6 +0,0 @@
-cd /home/icotrust
-git pull
-npm i
-systemctl restart icotrust
-curl 'https://api.telegram.org/bot237463370:AAGI_aywb5T-oCyFaRHsgae5XhuEwWd257o/sendMessage?chat_id=-76104711&text=icotrust.review%20deployed!'
-
